024957bd5f5a11e5c5fc535ddf14c6a370dd6a4e
alvaro <alvarosanz122@gmail.com>
2026-09-04 14:39:10 +0200

    Descripción de los cambios

---
+++ controller/nav/header.php
@@ -0,0 +1,11 @@
+<header>
+<nav class="navbar bg-black">
+  <div class="container-fluid">
+    <a href="#" class="navbar-brand text-white pe-auto"><i class="bi bi-person-check fs-3 img-fluid"></i></a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success text-white" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+</header>
